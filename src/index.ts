859092409c381fb68d81ce61c9dc4d08db038364
Edward205204 <khoa123kd56cn@gmail.com>
2025-07-13 08:13:34 +0700

    feat: refactore socket io

--- src/index.ts
+++ src/index.ts
@@ -14,15 +14,8 @@ import bookmarksRouter from './routes/bookmarks.routes';
 import likesRouter from './routes/likes.routes';
 import searchRouter from './routes/search.routes';
 import conversationsRouter from './routes/conversations.routes';
-
 import { createServer } from 'http';
-import { Server } from 'socket.io';
-import { ObjectId } from 'mongodb';
-import Conversation from './models/schemas/Conversation.schema';
-import { accessTokenDecode } from './constants/regex';
-import { TokenPayload } from './models/schemas/requests/User.request';
-import { UserVerifyStatus } from './constants/enums';
-
+import { initSocket } from './utils/socket';
 // import './utils/fake';
 
 config();
@@ -30,87 +23,7 @@ config();
 const app = express();
 const httpServer = createServer(app);
 const PORT = process.env.PORT || 4000;
-
-const io = new Server(httpServer, {
-  cors: {
-    origin: 'http://localhost:3000'
-  }
-});
-
-const users: { [key: string]: { socket_id: string } } = {};
-
-io.use(async (socket, next) => {
-  try {
-    const { Authorization } = socket.handshake.auth;
-
-    if (!Authorization || !Authorization.startsWith('Bearer ')) {
-      throw new Error('Invalid token');
-    }
-
-    const token = Authorization.split(' ')[1];
-
-    const decoded_authorization = (await accessTokenDecode(token)) as TokenPayload;
-
-    console.log(decoded_authorization.verify == UserVerifyStatus.Unverified);
-
-    console.log(decoded_authorization.verify);
-    console.log(UserVerifyStatus.Verified);
-    if (decoded_authorization.verify !== UserVerifyStatus.Verified) {
-      throw new Error('User is not verified');
-    }
-
-    (socket.handshake as any).decoded_authorization = decoded_authorization;
-    socket.handshake.auth.access_token = token;
-    next();
-  } catch (error) {
-    next({ message: 'Unauthorized', name: 'Unauthorized', data: error });
-  }
-});
-
-io.on('connection', (socket) => {
-  const { user_id } = (socket.handshake as any).decoded_authorization;
-
-  users[user_id] = {
-    socket_id: socket.id
-  };
-
-  socket.use(async (packet, next) => {
-    const { access_token } = socket.handshake.auth;
-    try {
-      await accessTokenDecode(access_token);
-      next();
-    } catch (error) {
-      next(new Error('Unauthorized'));
-    }
-  });
-
-  socket.on('error', (error) => {
-    if (error.message === 'Unauthorized') {
-      socket.disconnect();
-    }
-  });
-
-  socket.on('send_message', async (data) => {
-    const socket_id_receiver = users[data.receiver_id]?.socket_id;
-    if (!socket_id_receiver) return;
-
-    const { receiver_id, content } = data;
-    const conversation = new Conversation({
-      sender_id: new ObjectId(user_id as string),
-      receiver_id: new ObjectId(receiver_id as string),
-      content
-    });
-
-    await databaseService.conversations.insertOne(conversation);
-    socket.to(socket_id_receiver).emit('receive_message', {
-      conversation
-    });
-  });
-  socket.on('disconnect', () => {
-    delete users[user_id];
-    console.log(`user ${socket.id} disconnected`);
-  });
-});
+initSocket(httpServer);
 
 initFolder();
 app.use(cors());
